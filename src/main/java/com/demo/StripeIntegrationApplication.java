0769bec64aceacc572ff68fcba1fd061662627ca
Vivek-Aghera <vivekaghera02@gmail.com>
2023-04-03 00:05:26 +0530

    first commit

---
+++ src/main/java/com/demo/StripeIntegrationApplication.java
@@ -0,0 +1,22 @@
+package com.demo;
+
+import java.util.StringTokenizer;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.stripe.exception.StripeException;
+
+@SpringBootApplication
+public class StripeIntegrationApplication {
+
+	public static void main(String[] args) throws StripeException {
+		StringTokenizer st = new StringTokenizer("My name is vivek.");
+
+		System.out.println(st.toString());
+		
+		while(st.hasMoreTokens()) {
+			System.out.println(st.nextToken());
+		}
+	}
+
+}
